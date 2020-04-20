28cd5d73c1604cc6eba553ff7281499dcfbd0d08
Gaby Mendez <gmendez90@gmail.com>
2020-04-20 13:53:34 -0400

    Finish WK2 objects-and-methods/exercise-1

--- objects-and-methods/exercise-1/lib/trick_or_treater.rb
+++ objects-and-methods/exercise-1/lib/trick_or_treater.rb
@@ -1,10 +1,38 @@
-class TrickOrTreater
+require_relative 'bag'
 
-  def initialize(costume_instance)
-    @costume_instance = costume_instance
+class TrickOrTreater
+  attr_reader :bag, :costume
+  def initialize(costume)
+    # this only works to reference the
+    # costume class created in 'trick_or_treater.rb'
+    # if it uses the variable costume.
+    # what if there's another costume
+    # called 'costume2' ?
+    @costume = costume
+    @bag = Bag.new
   end
 
   def dressed_up_as
-    @costume_instance.style
+    @costume.style
+  end
+
+  def has_candy?
+    if bag.count <= 0
+      false
+    else
+      true
+    end
+  end
+
+  def candy_count
+    bag.count
+  end
+
+  def eat
+    bag.count -= 1
   end
 end
+
+
+# gaby = TrickOrTreater.new("alien")
+# p gaby.bag.empty?
